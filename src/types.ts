2a4fdfa6ebcea8e7e1768d1d5efd853194c5fba3
Virsaviya <14098014+virsaviya@users.noreply.github.com>
2024-12-16 11:48:21 -0800

    Add content

---
+++ src/types.ts
@@ -0,0 +1,42 @@
+export interface Education_t {
+  degree: string;
+  institution: string;
+  location: string;
+  startDate: Date;
+  endDate: Date;
+}
+
+export interface Header_t {
+  name: string;
+  position: string;
+  phone: string;
+  email: string;
+  location: string;
+  linkedin: string;
+  github?: string;
+}
+
+export interface History_t {
+  id: string;
+  organization: string;
+  position: string;
+  location: string;
+  startDate: Date;
+  endDate?: Date;
+  stack?: Array<string>; // @TODO create icon enum
+}
+
+export interface Skills_t {
+  libraries: Array<string>;
+  frameworks: Array<string>;
+  methodologies: Array<string>;
+  design: Array<string>;
+}
+
+export interface Strengths_t {
+  id: string;
+  title: string;
+  outcomes: Array<string>;
+}
+
+export type Summary_t = string;
