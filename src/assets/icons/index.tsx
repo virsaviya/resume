782486c0171f211bd83032cc05e84bb0c2745ed2
Virsaviya <14098014+virsaviya@users.noreply.github.com>
2024-12-17 14:21:14 -0800

    Add Github, Gmail, Linkedn, Location, and Phone Icons

--- src/assets/icons/index.tsx
+++ src/assets/icons/index.tsx
@@ -2,11 +2,16 @@ import { ReactComponent as Accenture } from './Accenture.svg';
 import { ReactComponent as ActNow } from './ActNow.svg';
 import { ReactComponent as CSS } from './CSS.svg';
 import { ReactComponent as Default } from './Default.svg';
+import { ReactComponent as Github } from './Github.svg';
+import { ReactComponent as Gmail } from './Gmail.svg';
 import { ReactComponent as GraphQL } from './GraphQL.svg';
 import { ReactComponent as HackerOne } from './HackerOne.svg';
 import { ReactComponent as JavaScript } from './JavaScript.svg';
+import { ReactComponent as Linkedin } from './Linkedin.svg';
+import { ReactComponent as Location } from './Location.svg';
 import { ReactComponent as LWT } from './LWT.svg';
 import { ReactComponent as Node } from './Node.svg';
+import { ReactComponent as Phone } from './Phone.svg';
 import { ReactComponent as React } from './React.svg';
 import { ReactComponent as Redux } from './Redux.svg';
 import { ReactComponent as Sigma } from './Sigma.svg';
@@ -24,18 +29,23 @@ export type IconName_t = keyof typeof iconMap;
 
 export const iconMap: Record<string, React.FC<IconProps_t>> = {
   Accenture,
+  ActNow,
   CSS,
   Default,
+  Github,
+  Gmail,
   GraphQL,
   HackerOne,
   JavaScript,
+  Linkedin,
+  Location,
+  LWT,
   Node,
+  Phone,
   React,
   Redux,
   Sigma,
   Storybook,
   TypeScript,
   Webpack,
-  LWT,
-  ActNow,
 };
--- src/components/Icon.tsx
+++ src/components/Icon.tsx
@@ -1,19 +1,22 @@
 import React from 'react';
 
+import { ReactComponent as Default } from 'assets/icons/Default.svg';
 import { IconProps_t, IconName_t, iconMap } from 'assets/icons';
+import { capitalize } from 'utils';
 
 export default function Icon({
   iconName,
   className,
+  size = 24,
   ...rest
 }: IconProps_t & {
   iconName: IconName_t;
 }): React.ReactElement | null {
-  const IconToRender = iconMap[iconName];
+  const IconToRender = iconMap[capitalize(iconName)] || Default;
   const props = {
     ...rest,
-    width: rest.size,
-    height: rest.size,
+    width: size,
+    height: size,
   };
   return (
     <div className={className}>
