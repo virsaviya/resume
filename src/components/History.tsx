86d36fca45bfa7003cb4a28562927d0088cf3f8c
Virsaviya <14098014+virsaviya@users.noreply.github.com>
2024-12-17 14:53:33 -0800

    Cleanup styles (and tweak history)

--- src/components/History.tsx
+++ src/components/History.tsx
@@ -2,24 +2,31 @@ import React from 'react';
 
 import Icon from 'components/Icon';
 import { History_t } from 'types';
-import { capitalize } from 'utils';
+import { parseDate } from 'utils';
 
 export default function History({ content }: { content: Array<History_t> }) {
   return (
     <div className='timeline'>
       {content.map((item) => {
+        const date = item.endDate
+          ? `${parseDate(item.startDate)} - ${parseDate(item.endDate)}`
+          : parseDate(item.startDate);
+
         return (
           <div className='timeline-event' key={item.id}>
-            <Icon
-              className='icon'
-              iconName={capitalize(item.id) || 'Default'}
-              size={20}
-              color='white'
-            />
-            <div>{item.organization}</div>
-            <div>{item.position}</div>
-            <div>2018 - 2020</div>
-            <div>stack here</div>
+            <Icon className='icon' iconName={item.id} size={20} color='white' />
+            <div className='deets'>
+              <h3>{item.organization}</h3>
+              <div>
+                <p>{item.position}</p>
+                <p>{date}</p>
+              </div>
+              <div className='stack'>
+                {item.stack?.map((lib) => (
+                  <Icon iconName={lib} />
+                ))}
+              </div>
+            </div>
           </div>
         );
       })}
